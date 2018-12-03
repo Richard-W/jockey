6f079f892bcdd99b83c2a856ad0c02eb2143f68e
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2018-12-03 08:40:15 +0100

    Add documentation

--- jockey/src/arguments.rs
+++ jockey/src/arguments.rs
@@ -1,6 +1,23 @@
+/// Represents a set of command line arguments that can be parsed or emitted
+///
+/// This trait is typically implemented by `#[derive(JockeyArguments)]`. The types
+/// currently supported in this derivation are
+///
+/// * String - Which is a mandatory option
+/// * Option<String> - Which is an optional option
+/// * bool - Which is a flag
+///
+/// The derivation macro also requires a static `new()` function that requires
+/// no further arguments and initializes `Option<String>` fields to `None` and
+/// `bool` fields to `false`.
 pub trait Arguments {
 
+    /// Emits a set of command line arguments.
     fn to_args(self) -> Vec<String>;
 
+    /// Parses command line arguments.
+    ///
+    /// This function expects a vector as supplied by `env::args().collect()`. It
+    /// ignores the first element which typically contains the path of the executable.
     fn parse_args(args: Vec<String>) -> Self;
 }
