548b9328eae2a1dffcc70f8ded16702f9dbc175b
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2018-12-04 21:29:34 +0100

    Update documentation

--- jockey/src/arguments.rs
+++ jockey/src/arguments.rs
@@ -1,15 +1,15 @@
 use result::Result;
 
-/// Represents a set of command line arguments that can be parsed or emitted.
+/// Represents a set of command line arguments that can be parsed.
 ///
 /// The easiest way to get this up and running is to add `#[derive(Default, Arguments)]`
 /// to the struct containing your argument data. For the derivation to work the struct may only
-/// contain types that implement both the `Parsable` and `Emittable` traits.
+/// contain types that implement `Parsable`.
 pub trait Arguments : Sized + Default {
 
     /// Parses command line arguments.
     ///
-    /// This function expects a vector as supplied by `env::args().collect()`. It
-    /// ignores the first element which typically contains the path of the executable.
+    /// This function expects an iterator as supplied by `env::args()`. The first element which
+    /// typically contains the path of the executable is ignored.
     fn parse_args<I> (args: I) -> Result<Self> where I : Iterator<Item = String>;
 }
