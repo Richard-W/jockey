3baa7dc86926255c7e1f19a4e2077bff98997843
Richard Wiedenhöft <richard@wiedenhoeft.xyz>
2018-12-05 20:07:40 +0100

    Fix example formatting

--- jockey_tests/src/main.rs
+++ jockey_tests/src/main.rs
@@ -4,13 +4,14 @@ extern crate jockey_derive;
 
 #[derive(Default, Arguments, Debug)]
 struct MyArguments {
-	pub string: Option<String>,
-	pub flag: bool,
+    pub string: Option<String>,
+    pub flag: bool,
+    pub multi: Vec<String>,
 }
 
 fn main() {
-	let args = <MyArguments as jockey::Arguments>::parse_args(std::env::args())
-            .expect("Error parsing command-line");
+    let args = <MyArguments as jockey::Arguments>::parse_args(std::env::args())
+        .expect("Error parsing command-line");
 
-        println!("{:#?}", args);
+    println!("{:#?}", args);
 }
